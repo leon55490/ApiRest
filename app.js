0f1425de6c8fc87d349be81bf07f5662b432ed82
David Rincon Toro <leon55490@gmail.com>
2023-02-12 22:20:05 -0500

    avance 1.1

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ const config = require('config');
 const app = express();
 const port = config.get('server-port');
 
+/** Methods */
+app.get('/', (req, res, next) => {
+	res.send('Welcome to academic rest api');
+});
+
+const controller = require('./controller/logic/student.controller');
+
+app.get('/student', (req, res, next) => {
+	controller.getAll(req, res, next);
+});
+
 app.listen(port, () => {
 	console.log('server is running on port');
 });
